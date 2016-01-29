2d8d7ce323ad20c953c72eb153fcb783983bd4eb
Tweak <pavel.starodubtsev@mail.ru>
2016-01-29 14:00:58 +0500

    Update JAVAHELLOWORLD.java
    hello world=privet mir

--- JAVAHELLOWORLD/src/javahelloworld/JAVAHELLOWORLD.java
+++ JAVAHELLOWORLD/src/javahelloworld/JAVAHELLOWORLD.java
@@ -15,7 +15,7 @@ public class JAVAHELLOWORLD {
      * @param args the command line arguments
      */
     public static void main(String args[]) {
-        System.out.print("Hello World!")
+        System.out.print("PRIVET MIR!")
         // TODO code application logic here
     }
     
